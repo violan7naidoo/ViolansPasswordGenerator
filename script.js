a8a5f4089396f090036cc01cdfd062d60cdb25fc
Violan <IT2@hwm.local>
2023-04-11 12:17:08 +0200

    initial

---
+++ script.js
@@ -0,0 +1,47 @@
+function generatePassword() {
+  // Get the user's input password
+  let password = document.getElementById("password").value;
+
+  // Generate a safer password using some algorithm
+  let saferPassword = "";
+
+  for (let i = 0; i < password.length; i++) {
+    switch (password[i]) {
+      case "a":
+      case "A":
+        saferPassword += "@";
+        break;
+      case "e":
+      case "E":
+        saferPassword += "3";
+        break;
+      case "s":
+      case "S":
+        saferPassword += "$";
+        break;
+      case "i":
+      case "I":
+        saferPassword += "!";
+        break;
+      case "o":
+      case "O":
+        saferPassword += "0";
+        break;
+      case " ":
+        saferPassword += "_";
+        break;
+      // add more cases for other letters
+      default:
+        saferPassword += password[i];
+        break;
+    }
+  }
+
+  // Set the output box to the safer password
+  document.getElementById("output").value = saferPassword;
+}
+
+function resetForm() {
+  document.getElementById("password").value = "";
+  document.getElementById("output").value = "";
+}
